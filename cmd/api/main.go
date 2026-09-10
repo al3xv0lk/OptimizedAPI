62032997860a0c1e5d8e7574d5307fd33e0dc4bb
Volk <ra.sendtko@gmail.com>
2026-09-10 15:07:13 -0300

    Graceful shutdown - no more race condition

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -22,22 +22,29 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  15 * time.Second,
 	}
+
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
 	go func() {
 		log.Println("Server running on 127.0.0.1:8080")
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("Listen error: %s\n", err)
 		}
+		serverStopCtx()
 	}()
 
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 	<-stop
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("Serve forced to shutdown: %v", err)
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
 	}
+
+	<-serverCtx.Done()
 	log.Println("Server stopped gracefully.")
 }
